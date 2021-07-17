5b3c1b2b20b2da9e8a554a44f231f1b747527dd2
Kamenchukov <k.kamenchukov@gmail.com>
2021-07-17 11:54:04 +0300

    HW_2_url session

---
+++ VkApp/Controllers/WKViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WKViewController.swift
+//  VkApp
+//
+//  Created by Константин Каменчуков on 16.07.2021.
+//
+
+import UIKit
+
+class WKViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VkApp/Service/VkService.swift
@@ -0,0 +1,8 @@
+//
+//  VkService.swift
+//  VkApp
+//
+//  Created by Константин Каменчуков on 16.07.2021.
+//
+
+import Foundation
