befd43a8acb2cde03db58fa369faf6035580c07d
Kamenchukov <k.kamenchukov@gmail.com>
2021-07-17 11:57:24 +0300

    hw_2

--- VkApp/Controllers/SecondViewController.swift
+++ VkApp/Controllers/SecondViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    let vkService = VkService(session: Session.shared)
+    
+    let errorMessage = "Ошибка"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        vkService.getUserInfo() { json in
+            print("Информация обо мне")
+            print(self.stringify(json) ?? self.errorMessage)
+        }
+        
+        vkService.getFriendsList() { json in
+            print("Список друзей")
+            print(self.stringify(json) ?? self.errorMessage)
+        }
+        
+        vkService.getPhotos() { json in
+            print("Мои фото")
+            print(self.stringify(json) ?? self.errorMessage)
+        }
+        
+        vkService.getGroups() { json in
+            print("Мои группы")
+            print(self.stringify(json) ?? self.errorMessage)
+        }
+        
+    }
+    
+    func stringify(_ json: Any?) -> String? {
+        guard let json = json else {
+            return nil
+        }
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: json, options: JSONSerialization.WritingOptions.prettyPrinted)
+            if let string = String(data: data, encoding: String.Encoding.utf8) {
+                return string
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
